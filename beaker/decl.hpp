918eea38bce03b9ed5ecb6d4f5ba9b3d81a3f5ad
Thomas O'Neill <toneill818@gmail.com>
2015-11-20 11:46:54 -0500

    Settup a pointer to a base class in the record declaration

--- beaker/decl.hpp
+++ beaker/decl.hpp
@@ -146,8 +146,8 @@ struct Parameter_decl : Decl
 // member lookup.
 struct Record_decl : Decl
 {
-  Record_decl(Symbol const* n, Decl_seq const& f, Decl_seq const& m)
-    : Decl(n, nullptr), fields_(f), members_(m), scope_(this)
+  Record_decl(Symbol const* n, Decl_seq const& f, Decl_seq const& m, Record_decl const* base)
+    : Decl(n, nullptr), fields_(f), members_(m), scope_(this), base_(base)
   { }
 
   void accept(Visitor& v) const { v.visit(this); }
@@ -162,6 +162,7 @@ struct Record_decl : Decl
   Decl_seq fields_;
   Decl_seq members_;
   Scope    scope_;
+    const Record_decl* base_;
 };
 
 
